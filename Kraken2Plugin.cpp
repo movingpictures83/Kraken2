57dc94b67556aec13f76ecf46b356d19566fe168
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2025-03-21 11:09:34 -0400

    Replaced with readParameterFile

--- Kraken2Plugin.cpp
+++ Kraken2Plugin.cpp
@@ -5,17 +5,18 @@
 
 void Kraken2Plugin::input(std::string file) {
   inputfile = file;
- std::ifstream ifile(inputfile.c_str(), std::ios::in);
+ /*std::ifstream ifile(inputfile.c_str(), std::ios::in);
  while (!ifile.eof()) {
-   /*std::string key, value;
-   ifile >> key;
-   ifile >> value;
-   parameters[key] = value;*/
+   //std::string key, value;
+   //ifile >> key;
+   //ifile >> value;
+   //parameters[key] = value;
 	 std::string key;
    ifile >> key;
    if (key != "")
    files.push_back(key);
- }
+ }*/
+  readParameterFile(file);
 }
 
 void Kraken2Plugin::run() {
@@ -29,12 +30,14 @@ void Kraken2Plugin::output(std::string file) {
 	//if (parameters.count("assembly") != 0) {
         //   command += " " + std::string(PluginManager::prefix())+"/"+parameters["assembly"];
 	//}
-	std::string command = "kraken2 --use-names --db ${KRAKEN2_DB} --report-minimizer-data --output "+file+" ";
-	if (files.size() % 2 == 0)
-		command += "--paired ";
-	for (int i=0; i < files.size(); i++){
-           command += /*std::string(PluginManager::prefix())+"/"+*/files[i] + " ";
-	}
+//--db $DB_PATH --report-zero-counts --threads $THREADS --report $KRAKEN_REPORT --paired $M1 $M2 > $KRAKEN_OUT
+        std::string command = "kraken2 --db "+myParameters["database"];
+        if (myParameters.count("reportzerocounts") != 0)
+		command += " --report-zero-counts ";
+	command += " --threads "+myParameters["threads"];
+	command += " --report "+PluginManager::addPrefix(myParameters["report"]);
+	command += " --paired "+PluginManager::addPrefix(myParameters["fastq1"])+" "+PluginManager::addPrefix(myParameters["fastq2"]);
+	command += " > "+file;
  std::cout << command << std::endl;
 
  system(command.c_str());
